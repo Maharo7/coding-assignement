544278932c80f82f391bf89073e8d70480dac96d
Maharo <rivomahefamaharo@gmail.com>
2023-07-24 19:32:14 +0300

    header and list component
    Creation header and list component
    nothing on them for now
    add developpement config on angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BackendService } from './backend.service';
+import { HeaderComponent } from './header/header.component';
+import {ButtonModule} from 'primeng/button';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { ListTicketComponent } from './list-ticket/list-ticket.component';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
+    declarations: [AppComponent, HeaderComponent, ListTicketComponent],
+    imports: [BrowserModule,ButtonModule,TabMenuModule,FieldsetModule],
     providers: [BackendService],
     bootstrap: [AppComponent]
 })
